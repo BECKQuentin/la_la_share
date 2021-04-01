ed1bbf6e12ba7ac56ddefa001d8c911c7b9f4de5
Quentin <beckquentin26@gmail.com>
2021-04-01 14:07:57 +0200

    slide

--- public/js/script.js
+++ public/js/script.js
@@ -27,17 +27,45 @@ $(function(){
     });
 
     /////////HIDE MUSICS///////
-    $('.musics_slide').on('click', function() {   
+    $('.musics_slide').on('click', function() {
+        $('.main').removeClass('col-10')   
         $('.musics_slide i').toggleClass('fa-chevron-left'); 
         $('.musics_slide i').toggleClass('fa-chevron-right'); 
-        $('.musics_box').toggleClass('show');               
+        $('.musics_box').toggleClass('show');
+        $('.musics_box').toggleClass('hide'); 
+        window.setTimeout(function() {
+            $('.main').toggleClass('offset-lg-2') 
+            $('.main').toggleClass('offset-lg-1')
+            $('.main').toggleClass('col-8')
+            $('.main').toggleClass('col-9') 
+        }, 100);
+        ////////HIDE MUSICS && FRIENDS////////
+        if( ($('.friends_box').hasClass('hide')) && ($('.musics_box').hasClass('hide')) ) {
+            $('.main').addClass('col-10')
+            $('.main').removeClass('col-8') 
+        }   
+                       
     });
-    /////////HIDE MUSICS///////
-    $('.friends_slide').on('click', function() {   
+    /////////HIDE FRIENDS///////
+    $('.friends_slide').on('click', function() {
+        $('.main').removeClass('col-10')   
         $('.friends_slide i').toggleClass('fa-chevron-left'); 
         $('.friends_slide i').toggleClass('fa-chevron-right'); 
-        $('.friends_box').toggleClass('show');               
+        $('.friends_box').toggleClass('show'); 
+        $('.friends_box').toggleClass('hide'); 
+        window.setTimeout(function() {
+            $('.main').toggleClass('col-8')
+            $('.main').toggleClass('col-9') 
+        }, 100); 
+        ////////HIDE MUSICS && FRIENDS////////
+        if( ($('.friends_box').hasClass('hide')) && ($('.musics_box').hasClass('hide')) ) {
+            $('.main').addClass('col-10')
+            $('.main').removeClass('col-8')            
+        }
+             
     });
+   
+
     
 
 })//jquery
