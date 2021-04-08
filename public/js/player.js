0246fca994b4db36ae5a05911ff27856a0ce9e25
Quentin <beckquentin26@gmail.com>
2021-04-08 17:07:08 +0200

    'audio'

--- public/js/player.js
+++ public/js/player.js
@@ -10,19 +10,31 @@ $(function(){
             $('.player_musics').css('display', 'flex');
         }
 
-        
         musicsAudio = $(this).data('musicsAudio');        
         musicsImage = $(this).data('musicsImage');
         musicsArtist = $(this).data('musicsArtist');
         musicsTitle = $(this).data('musicsTitle');
 
         //changer le src des balises audio et image
-        $('.player_musics_audio source').attr('src', musicsAudio);
         $('.player_musics_img img').attr('src', musicsImage);        
-        $('.player_musics_title_artist p').text(musicsArtist+' - '+musicsTitle);  
-        
-        $('.player_musics_audio audio').load();
-    
+        $('.player_musics_audio audio').attr('src', musicsAudio);        
+        $('.player_musics_title_artist p').text(musicsArtist+' - '+musicsTitle); 
+
+        $('#player')[0].load();
+        setTimeout(() => {
+            $('#player')[0].play();
+            $('#player').prop('volume', 0.3);
+        }, 800);
+
+        // var html = `<audio controls preload="auto" id="player">
+        //                 <source src="${musicsAudio}" type="audio/mp3">
+        //             </audio>`;
+        // $('.player_musics_audio').html(html);
+
+        // setTimeout(() => {            
+        //     $('#player')[0].play();
+        //     $('.audio').prop(volume, 0.5);
+        // }, 500);
     });
 
     ////////CLOSE PLAYER//////  
